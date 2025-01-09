4a47e0eff303a92591394c84f2b608d8677ef758
PhaNTa <alex2009oks@gmail.com>
2025-01-09 16:43:16 +1000

    Breaks

--- main.py
+++ main.py
@@ -3,8 +3,27 @@ from time import sleep
 import pygame
 import os
 
+
 class Sprites(pygame.sprite.Sprite):
-    pass
+    def __init__(self, name, height, width):
+        super().__init__()
+        self.image = pygame.image.load(os.path.join(os.getcwd(), name)).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (height, width))
+        self.rect = self.image.get_rect()
+        WIDTH, HEIGHT = self.image.get_size()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def moveRight(self, pixels):
+        self.rect.x += pixels
+
+    def moveLeft(self, pixels):
+        self.rect.x -= pixels
+
+    def moveForward(self, pixels):
+        self.rect.y += pixels
+
+    def moveBack(self, pixels):
+        self.rect.y -= pixels
 
 class Player(Sprites):
     pass
@@ -13,11 +32,11 @@ class Player(Sprites):
 class Item(Sprites):
     pass
 
+
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Pink Panther: Old Friend')
-    current_dir = os.getcwd()
-    os.chdir(f'{current_dir}\\assets')
+    os.chdir(f'{os.getcwd()}\\assets')
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     screen.fill('black')
     pygame.display.update()
@@ -39,18 +58,17 @@ def menu():
 
 
 def intro(screen):
+    global player_imag_ch2, player_imag_ch1
     while True:
-        visible_area = screen.get_size()
         x = 50
         y = 500
 
-        for image in os.curdir:
-            current_path = os.path.dirname(__file__)
-            image_path = os.path.join(current_path, 'assets')
-            player_image = pygame.image.load(os.path.join(image_path, 'pink_paw.png')).convert_alpha()
-            player_imag_ch = pygame.transform.scale(player_image, (125, 125))
-            player_imag_ch1 = pygame.transform.rotate(player_imag_ch, -40)
-            player_imag_ch2 = pygame.transform.rotate(player_imag_ch, -10)
+        current_path = os.path.dirname(__file__)
+        image_path = os.path.join(current_path, 'assets')
+        player_image = pygame.image.load(os.path.join(image_path, 'pink_paw.png')).convert_alpha()
+        player_imag_ch = pygame.transform.scale(player_image, (125, 125))
+        player_imag_ch1 = pygame.transform.rotate(player_imag_ch, -40)
+        player_imag_ch2 = pygame.transform.rotate(player_imag_ch, -10)
 
         for j in range(5):
             screen.blit(player_imag_ch1, (x, y))
@@ -64,12 +82,26 @@ def intro(screen):
             y -= 20
             sleep(0.5)
 
+        panther_intro = Sprites('panther_intro.png', 200, 200)
+        panther_intro.rect.x = 1900
+        panther_intro.rect.y = 910
+
+        for _ in range(10):
+            all_sprites.add(panther_intro)
+            panther_intro.moveLeft(10)
+            all_sprites.update()
+            all_sprites.draw(screen)
+            pygame.display.update()
+            clock.tick(60)
+        break
+    return
+
 
 while True:
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
     intro(screen)
-    menu()
-    '''https://lms.yandex.ru/courses/1180/groups/23822/lessons/6992/materials/20596'''
-    '''СДЕЛАТЬ НОВЫЙ РЕПОЗИТОРИЙ И ДИРЕКТОРИЮ!'''
+    sleep(5)
+    exit()

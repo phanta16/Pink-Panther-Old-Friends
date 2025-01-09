58acae53e5153f280b4c0a6ad78b6febb9bfc86c
PhaNTa <alex2009oks@gmail.com>
2025-01-09 20:33:17 +1000

    Intro

--- main.py
+++ main.py
@@ -4,32 +4,32 @@ import pygame
 import os
 
 
-class Sprites(pygame.sprite.Sprite):
-    def __init__(self, name, height, width):
-        super().__init__()
-        self.image = pygame.image.load(os.path.join(os.getcwd(), name)).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (height, width))
-        self.rect = self.image.get_rect()
-        WIDTH, HEIGHT = self.image.get_size()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+# class Sprites(pygame.sprite.Sprite):
+#     def __init__(self, name, height, width):
+#         super().__init__()
+#         self.image = pygame.image.load(os.path.join(os.getcwd(), name)).convert_alpha()
+#         self.image = pygame.transform.scale(self.image, (height, width))
+#         self.rect = self.image.get_rect()
+#         WIDTH, HEIGHT = self.image.get_size()
+#         self.rect.center = (WIDTH / 2, HEIGHT / 2)
+#
+#     def moveRight(self, pixels):
+#         self.rect.x += pixels
+#
+#     def moveLeft(self, pixels):
+#         self.rect.x -= pixels
+#
+#     def moveForward(self, pixels):
+#         self.rect.y += pixels
+#
+#     def moveBack(self, pixels):
+#         self.rect.y -= pixels
 
-    def moveRight(self, pixels):
-        self.rect.x += pixels
-
-    def moveLeft(self, pixels):
-        self.rect.x -= pixels
-
-    def moveForward(self, pixels):
-        self.rect.y += pixels
-
-    def moveBack(self, pixels):
-        self.rect.y -= pixels
-
-class Player(Sprites):
+class Player(pygame.sprite.Sprite):
     pass
 
 
-class Item(Sprites):
+class Item(pygame.sprite.Sprite):
     pass
 
 
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     pygame.display.update()
     pygame.mixer.init()
     clock = pygame.time.Clock()
-    all_sprites = pygame.sprite.Group()
+    intro_sprites = pygame.sprite.Group()
 
 
 def game():
@@ -58,14 +58,20 @@ def menu():
 
 
 def intro(screen):
-    global player_imag_ch2, player_imag_ch1
+
+
     while True:
+
+        current_path = os.path.dirname(__file__)
+        assets_path = os.path.join(current_path, 'assets')
+
+        pygame.mixer.music.load('intro_song.mp3')
+        pygame.mixer.music.play()
+
         x = 50
         y = 500
 
-        current_path = os.path.dirname(__file__)
-        image_path = os.path.join(current_path, 'assets')
-        player_image = pygame.image.load(os.path.join(image_path, 'pink_paw.png')).convert_alpha()
+        player_image = pygame.image.load(os.path.join(assets_path, 'pink_paw.png')).convert_alpha()
         player_imag_ch = pygame.transform.scale(player_image, (125, 125))
         player_imag_ch1 = pygame.transform.rotate(player_imag_ch, -40)
         player_imag_ch2 = pygame.transform.rotate(player_imag_ch, -10)
@@ -75,24 +81,52 @@ def intro(screen):
             pygame.display.flip()
             x += 210
             y -= 20
-            sleep(0.5)
+            sleep(0.8)
             screen.blit(player_imag_ch2, (x, y))
             pygame.display.flip()
             x += 210
             y -= 20
-            sleep(0.5)
+            sleep(0.8)
 
-        panther_intro = Sprites('panther_intro.png', 200, 200)
-        panther_intro.rect.x = 1900
-        panther_intro.rect.y = 910
+        panther_intro = pygame.sprite.Sprite(intro_sprites)
+        panther_intro.image = pygame.transform.scale(
+            pygame.image.load(os.path.join(assets_path, 'panther_intro.png')).convert_alpha(), (400, 400))
+        panther_intro.rect = panther_intro.image.get_rect()
+        panther_intro.rect.x = 1700
+        panther_intro.rect.y = 690
 
-        for _ in range(10):
-            all_sprites.add(panther_intro)
-            panther_intro.moveLeft(10)
-            all_sprites.update()
-            all_sprites.draw(screen)
+        for _ in range(20):
+            panther_intro.rect.x -= 5
+            intro_sprites.update()
+            intro_sprites.draw(screen)
             pygame.display.update()
             clock.tick(60)
+            screen.fill('BLACK')
+        sleep(3)
+        for _ in range(40):
+            panther_intro.rect.x += 10
+            intro_sprites.update()
+            intro_sprites.draw(screen)
+            pygame.display.update()
+            clock.tick(60)
+            screen.fill('BLACK')
+
+        logo = pygame.sprite.Sprite(intro_sprites)
+        logo.image = pygame.image.load(os.path.join(assets_path, 'panther_logo.png')).convert_alpha()
+        logo.rect = logo.image.get_rect()
+        logo.rect.x = 200
+        logo.rect.y = -10
+
+        sleep(0.5)
+
+        for _ in range(350):
+            logo.rect.y += 1
+            intro_sprites.update()
+            intro_sprites.draw(screen)
+            pygame.display.update()
+            clock.tick(60)
+            screen.fill('BLACK')
+
         break
     return
 
